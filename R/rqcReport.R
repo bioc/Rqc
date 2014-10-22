8e995bb7814605b5841781b448c11a0ff3295133
Benilton Carvalho <bcarvalh@jhsph.edu>
2014-10-22 18:49:46 +0000

    setting up git-svn bridge
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rqc@95892 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rqcReport.R
+++ R/rqcReport.R
@@ -2,6 +2,7 @@ rqcReport <- function(rqcResultSet, outdir=tempdir(), file="rqc_report")
 {
     outdir <- normalizePath(outdir)
     output <- file.path(outdir, paste0(file, '.html'))
+    figDir <- paste0(tempdir(), "/rqc-")
     rmdFile <- system.file("templates", package="Rqc", "rqc_report.Rmd")
     knit2html(rmdFile, output, quiet = TRUE, options=c("base64_images"))
     output
