4e0ef21e1bed9907db8eb90b84b2feaf8afa7797
Benilton Carvalho <bcarvalh@jhsph.edu>
2015-04-08 13:24:48 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: b3c48e078d1eedf3d59d495f11af2c5166af4df4
    
        Added NEWS file; PCA plot
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rqc@102084 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/rqcCycleAverageQualityPcaCalc.R
@@ -0,0 +1,7 @@
+rqcCycleAverageQualityPcaCalc <- function(rqcResultSet)
+{
+  df <- rqcCycleAverageQualityCalc(rqcResultSet)
+  df.wide <- dcast(df, cycle ~ filename, value.var = "quality")
+  fit <- prcomp(~ ., data=df.wide[,-1])
+  return(fit)
+}
---
+++ R/rqcCycleAverageQualityPcaPlot.R
@@ -0,0 +1,21 @@
+rqcCycleAverageQualityPcaPlot <- function(rqcResultSet)
+{
+    fit <- rqcCycleAverageQualityPcaCalc(rqcResultSet)
+    df <- data.frame(cycle=factor(1:nrow(fit$x)), fit$x)
+    pc <- data.frame(filename=rownames(fit$rotation), fit$rotation)
+    mult <- min(
+        (max(df$PC2) - min(df$PC2)/(max(pc$PC2)-min(pc$PC2))),
+        (max(df$PC1) - min(df$PC1)/(max(pc$PC1)-min(pc$PC1)))
+    )
+    pc <- transform(pc,
+        v1 = .7 * mult * (get("PC1")),
+        v2 = .7 * mult * (get("PC2"))
+    )
+    ggplot(df, aes_string(x="PC1", y="PC2")) + 
+        geom_text(aes_string(label="cycle")) +
+        geom_hline(aes(0), size=.2) + geom_vline(aes(0), size=.2) + 
+        geom_text(aes_string(x="v1", y="v2", label="filename"), vjust=1, color="red", pc) +
+        geom_segment(aes_string(x=0, y=0, xend="v1", yend="v2"), 
+            arrow=arrow(length=unit(0.2,"cm")), alpha=0.75, color="red", pc) +
+        theme_bw()
+}
