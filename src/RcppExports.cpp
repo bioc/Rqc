cf604641dd310b228445b906c30f03f7e743dded
Welliton Souza <well309@gmail.com>
2021-07-28 20:26:44 -0300

    Fix knitr issue

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // toRRDNA
 StringVector toRRDNA(StringVector dnas);
 RcppExport SEXP _Rqc_toRRDNA(SEXP dnasSEXP) {
